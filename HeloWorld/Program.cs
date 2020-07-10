df629f51166ea1d61e6a02fc34e5eaff44e0cc81
atjtiwari <atjtiwari@outlook.com>
2020-07-10 10:21:42 +0530

    First Push

---
+++ HeloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeloWorld
+{
+    class Program
+    {
+        public void HeloWorld()
+        {
+            Console.WriteLine("Helo World");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.HeloWorld();
+            Console.ReadLine();
+
+        }
+    }
+}
